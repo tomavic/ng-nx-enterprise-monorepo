311f1967efeb3e6f228a5d9e814a5a775ebc9699
tomavic <hbasheer@live.com>
2024-11-16 03:24:50 +0200

    chore(admin-portal): 🔧 minor updates

--- apps/admin-portal/src/app/app.routes.ts
+++ apps/admin-portal/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/router';
 
+import { HomeComponent } from './containers/home-page/Home.component';
 import { LoginComponent } from './containers/login-page/Login.component';
 import { NotFound4O4Component } from './containers/not-found-page/NotFound4O4.component';
 import { HomeGuard } from './guards/home.guard';
@@ -12,28 +13,40 @@ export const appRoutes: Route[] = [
     canActivate: [LoginGuard],
     component: LoginComponent
   },
+  /**
+   * All Pages that are commonly using the same Header and Footer,
+   * are grouped under the Home Layout
+   */
   {
     path: 'home',
     canActivate: [HomeGuard],
-    loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
-  },
-  // Option 1: Lazy Loading another Routing Config
-  // {
-  //   path: 'flight-booking',
-  //   loadChildren: () =>
-  //     import('./booking/flight-booking.routes').then(
-  //       (m) => m.FLIGHT_BOOKING_ROUTES
-  //     ),
-  // },
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+      }
 
-  // Option 2: Directly Lazy Loading a Standalone Component
-  // {
-  //   path: 'next-flight',
-  //   loadComponent: () =>
-  //     import('./next-flight/next-flight.component').then(
-  //       (m) => m.NextFlightComponent
-  //     ),
-  // },
+      // Option 1: Lazy Loading another Routing Config
+      // {
+      //   path: 'flight-booking',
+      //   loadChildren: () =>
+      //     import('./booking/flight-booking.routes').then(
+      //       (m) => m.FLIGHT_BOOKING_ROUTES
+      //     ),
+      // },
+
+      // Option 2: Directly Lazy Loading a Standalone Component
+      // {
+      //   path: 'next-flight',
+      //   loadComponent: () =>
+      //     import('./next-flight/next-flight.component').then(
+      //       (m) => m.NextFlightComponent
+      //     ),
+      // },
+    ]
+  },
+
   { path: 'not-found-404', component: NotFound4O4Component },
   { path: '**', redirectTo: 'not-found-404' }
 ];
